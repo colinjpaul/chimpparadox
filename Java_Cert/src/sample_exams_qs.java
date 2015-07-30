9629261968438f8fd1ff4efd822fb3b90b364ff4
colinjpaul <colinpaul@gmail.com>
2015-07-30 23:43:32 +0100

    now commiting from work

--- Java_Cert/src/sample_exams_qs.java
+++ Java_Cert/src/sample_exams_qs.java
@@ -7,7 +7,7 @@ public class sample_exams_qs {
 		System.out.println("thursday night and windows 10 "
 				+ "installed and driver issue looks to be fixed");
 			
-		
+		System.out.println("now changing from work laptop");
 	}
 
 }
