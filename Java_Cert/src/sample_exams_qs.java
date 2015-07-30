ce666314ad57be1858b9cc82357295140455079c
colinjpaul <colinpaul@gmail.com>
2015-07-30 13:30:23 +0100

    recent changes

--- Java_Cert/src/sample_exams_qs.java
+++ Java_Cert/src/sample_exams_qs.java
@@ -2,13 +2,10 @@
 public class sample_exams_qs {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("testing changes to git");
 		
-		
-		// Thursday morning and figuring github out
-		// some more edits from work laptop
-		// here we go again
+		byte foo = 120;
+		switch(foo)}{
+			
 		
 	}
 
