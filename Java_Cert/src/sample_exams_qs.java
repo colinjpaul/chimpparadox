11c7ef4a90636e1dd8b62dfe482f0722e85976c2
colinjpaul <colinpaul@gmail.com>
2015-07-30 08:53:21 +0100

    Further changes from work laptop

--- Java_Cert/src/sample_exams_qs.java
+++ Java_Cert/src/sample_exams_qs.java
@@ -4,7 +4,9 @@ public class sample_exams_qs {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("testing changes to git");
-		// Testing changes from work laptop
+		
+		
+		// Thursday morning and figuring github out
 		
 	}
 
