5ae43fc6733c6ebc652682603d47a2835e1867af
colinjpaul <colinpaul@gmail.com>
2015-08-06 13:27:39 +0100

    twist in the tale exercise

--- Java_Cert/src/twist_in_the_tales/Office.java
+++ Java_Cert/src/twist_in_the_tales/Office.java
@@ -1,10 +1,34 @@
 package twist_in_the_tales;
 
+class Employee {
+	String name;
+	String address;
+	protected String phoneNumber;
+	public float experience;
+}
+
+
+class Programmer extends Employee{
+	Programmer (String val){
+		name = val;
+	}
+	
+	String getName(){
+		return name;
+	}
+}
+
+
 public class Office {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	 String programmerName = new Programmer ("someoneelse").getName();
+	 
+	 System.out.println(programmerName);
+	 
+	
+		
 	}
 
 }
