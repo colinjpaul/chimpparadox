337a86aa6d8b23a5783ce6dab54531a6397dba5d
colinjpaul <colinpaul@gmail.com>
2015-08-06 20:58:23 +0100

    updating content from home

--- Java_Cert/src/sample_exam_questions/chapter_6.java
+++ Java_Cert/src/sample_exam_questions/chapter_6.java
@@ -4,7 +4,8 @@ public class chapter_6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//placeholder
 	}
 
 }
--- Java_Cert/src/twist_in_the_tales/Office.java
+++ Java_Cert/src/twist_in_the_tales/Office.java
@@ -4,6 +4,8 @@ public class Office {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//placeholder
 
 	}
 
