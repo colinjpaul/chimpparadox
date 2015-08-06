892511b31e625fd045c2a47af06d23e26bb47136
colinjpaul <colinpaul@gmail.com>
2015-08-06 20:46:17 +0100

    Revert "twist in the tale exercise"
    This reverts commit 5ae43fc6733c6ebc652682603d47a2835e1867af.

---
+++ Java_Cert/src/sample_exams_qs.java
@@ -0,0 +1,21 @@
+
+public class sample_exams_qs {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("thursday night and windows 10 "
+				+ "installed and driver issue looks to be fixed");
+			
+		System.out.println("now changing from work laptop");
+		
+		System.out.println("one final change from home laptop");
+		
+		//one final change
+		//maybe not - it's friday!!!
+		//friday evening and going to turn off laptop now
+		//inevitable teething problems with windows 10
+		//now on new profile in Windows 10
+		//updated from work laptop
+	}
+}
--- Java_Cert/src/twist_in_the_tales/Office.java
+++ Java_Cert/src/twist_in_the_tales/Office.java
@@ -1,34 +1,10 @@
 package twist_in_the_tales;
 
-class Employee {
-	String name;
-	String address;
-	protected String phoneNumber;
-	public float experience;
-}
-
-
-class Programmer extends Employee{
-	Programmer (String val){
-		name = val;
-	}
-	
-	String getName(){
-		return name;
-	}
-}
-
-
 public class Office {
 
 	public static void main(String[] args) {
-		
-	 String programmerName = new Programmer ("someoneelse").getName();
-	 
-	 System.out.println(programmerName);
-	 
-	
-		
+		// TODO Auto-generated method stub
+
 	}
 
 }
