a650e22603ba53af234dbee966a325525eb8f514
colinjpaul <colinpaul@gmail.com>
2015-08-07 08:53:56 +0100

    Completed twist in tale 6.1

--- Java_Cert/src/twist_in_the_tales/Office.java
+++ Java_Cert/src/twist_in_the_tales/Office.java
@@ -1,11 +1,35 @@
 package twist_in_the_tales;
 
+class Employee {
+	
+	String name;
+	String address;
+	protected String phoneNumber;  //can be accessed by all derived classes regarded of package
+	public float experience;
+	
+}
+
+class Programmer extends Employee{
+	Programmer (String val){
+		name = val;
+	}
+	
+	String getName(){
+		return name;
+	}
+}
+
+
 public class Office {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		//placeholder
+		String tester = new Programmer ("Harry").getName();
+		
+		System.out.println(tester);
+		
+		
+		
 
 	}
 
