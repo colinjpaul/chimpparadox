742f49213eea479073421866f541f4fefddf7d8a
colinjpaul <colinpaul@gmail.com>
2015-08-11 13:57:37 +0100

    golf stat stuff

---
+++ golfstat/src/golfstat/Bunkered.java
@@ -0,0 +1,19 @@
+package golfstat;
+
+import javax.swing.*;
+
+public class Bunkered {
+
+	public static void main(String[] args) {
+		
+				
+		JFrame frame = new JFrame( "CJ Golf App JFrame" );
+		JLabel label = new JLabel("CJ Golf App JLabel", JLabel.CENTER );
+	    frame.getContentPane().add( label );
+	    frame.setSize( 300, 300 );
+	    frame.setVisible( true );
+		
+
+	}
+
+}
