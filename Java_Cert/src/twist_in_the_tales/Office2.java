0a4fd09eb2d9bcf0f3e1b4316325fe21232d24fa
colinjpaul <colinpaul@gmail.com>
2015-10-13 22:07:30 +0100

    casting

---
+++ Java_Cert/src/twist_in_the_tales/Office2.java
@@ -0,0 +1,50 @@
+package twist_in_the_tales;
+
+class Employee {
+	
+	String name;
+	String address;
+	String phoneNumber;  
+	float experience;
+}
+
+interface Interviewer{
+	public void conductInterview();
+}
+
+
+class HRExecutive extends Employee implements Interviewer{
+
+	String[] specialization;
+	
+	public void conductInterview() {
+		System.out.println("HR Executive - Conducting Interview");
+	}
+	
+}
+
+class Manager implements Interviewer{
+	
+	int teamSize;
+		
+	public void conductInterview(){
+		System.out.println("Mgr - conductInterview");
+	}
+	
+	
+	
+}
+
+class Office2 {
+
+	public static void main(String[] args) {
+		
+		Interviewer interviewer = new HRExecutive();
+		
+		((HRExecutive)interviewer).specialization = new String[]{"Staffing"};
+		
+		
+	}
+}
+
+
