ef0aaed5c6d03e0960dcd18bac1c97252c29d3f4
colinjpaul <colinpaul@gmail.com>
2015-11-21 17:22:23 +0000

    changes from saturday

--- Java_Cert/src/exceptions/ThrowNumberFormatException.java
+++ Java_Cert/src/exceptions/ThrowNumberFormatException.java
@@ -1,13 +1,40 @@
 package exceptions;
 
+class MyException extends RuntimeException{}
+
 public class ThrowNumberFormatException {
 	
+	static void recursion(){
+		recursion();
+	}
+	
+	
+	/*
+	//  static initializer block
+	
+	static{
+		int num = Integer.parseInt("sd",16);
+	}
+	
+	//  Initialization of a static variable
+	static String name = null;
+	static int nameLength = name.length();
+	
+	
+	
+	static String name = getName();
+	
+	static String getName(){
+		throw new MyException();
+	}*/
+	
+		
 	public static int convertToNum(String val){
 		int num = 0;
 		try{
 			num = Integer.parseInt(val, 16);
 		}
-		catch(NumberFormatException e){
+		catch(NumberFormatption e){
 			throw new NumberFormatException(val+ " cannot be converted to hexadecimal number");
 			
 		}
@@ -15,6 +42,8 @@ public class ThrowNumberFormatException {
 	}
 	
 	public static void main(String[] args) {
+		
+		recursion();
 		
 		System.out.println(convertToNum("16b"));
 		System.out.println(convertToNum("16f"));
@@ -24,3 +53,5 @@ public class ThrowNumberFormatException {
 	}
 
 }
+
+
