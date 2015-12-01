85d129f64a2b11b732d88dac5775ff9eed149d5c
colinjpaul <colinpaul@gmail.com>
2015-12-01 21:24:21 +0000

    euler_puzzles

--- Puzzles/Euler/P1_multiples_of_3_and_5.py
+++ Puzzles/Euler/P1_multiples_of_3_and_5.py
@@ -1,1 +1,7 @@
-print("here we are - tis Saturday")
+def multiples_of_3_or_5():
+        for number in xrange(1000):
+            if not number % 3 or not number % 5:
+                yield number
+
+print sum(multiples_of_3_or_5())
+
