1860c28f264399a91be7c49df85a702ea361bddf
colinjpaul <colinpaul@gmail.com>
2015-12-02 13:05:56 +0000

    furtherchanges

--- Puzzles/Euler/P1_multiples_of_3_and_5.py
+++ Puzzles/Euler/P1_multiples_of_3_and_5.py
@@ -1,7 +1,8 @@
 def multiples_of_3_or_5():
-        for number in xrange(1000):
+        for number in xrange(10):
             if not number % 3 or not number % 5:
                 yield number
+                print number
 
 print sum(multiples_of_3_or_5())
 
