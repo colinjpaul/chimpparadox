e46291f430d53c8f6146076f774f21ba8f8d94d7
colinjpaul <colinpaul@gmail.com>
2015-12-03 21:27:47 +0000

    changed syntax for python 3

--- Puzzles/Euler/P1_multiples_of_3_and_5.py
+++ Puzzles/Euler/P1_multiples_of_3_and_5.py
@@ -1,4 +1,8 @@
-print ("from home")
+def multiples_of_3_or_5():
+    for number in range(10):
+        if not number % 3 or not number % 5:
+            yield number
+print(sum(multiples_of_3_or_5()))
 
 
 
--- PyPong/sandbox/cptest.py
+++ PyPong/sandbox/cptest.py
@@ -1,1 +1,4 @@
 print("test file")
+
+
+
