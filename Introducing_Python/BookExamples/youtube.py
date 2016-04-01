f97095f9d06342b5b3b9984db643c162eb1f881e
colinjpaul <colinpaul@gmail.com>
2016-04-01 14:57:23 +0100

    changes from 3rd edition of introducing python

--- Introducing_Python/BookExamples/youtube.py
+++ Introducing_Python/BookExamples/youtube.py
@@ -10,3 +10,7 @@ text = contents.decode('utf8')
 data = json.loads(text)
 for video in data['feed']['entry'][0:6]:
     print(video['title']['$t'])
+
+#This example no longer works - see note in page 4 of third edition of book
+
+
