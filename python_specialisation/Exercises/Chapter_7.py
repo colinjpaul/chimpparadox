e52ac467bc2f0f8465959792fbdd4e3eade49011
colinjpaul <colinpaul@gmail.com>
2016-06-23 13:28:38 +0100

    update from work

---
+++ python_specialisation/Exercises/Chapter_7.py
@@ -0,0 +1,12 @@
+text = open('C:\\Users\\paulc1\\Documents\\GitHub\\chimpparadox\\testfiles\\py_chapter7.txt', 'r')
+
+count = 0
+
+for word in text:
+    count += 1
+
+print (count)
+
+#Continue from 13:55
+
+
