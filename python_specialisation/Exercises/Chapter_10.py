067c0b6a4f042861121e44630c9012b6ee0e348b
colinjpaul <colinpaul@gmail.com>
2016-09-06 21:15:07 +0100

    more updates

--- python_specialisation/Exercises/Chapter_10.py
+++ python_specialisation/Exercises/Chapter_10.py
@@ -11,26 +11,21 @@ for line in fhand:
 print counts
 
 lst = list()
-
 for key, val in counts.items():
     lst.append((val, key))
 
-
 lst.sort(reverse=True)
-
-print lst
-
-#continue from 12:21 in video
-'''
 
 for val, key in lst[:10]:
     print key, val
 
 
-# Shorter version
+#Shorter Version
 
-c = {'a': 10, 'b': 1, 'c': 22}
+c = {'a': 10, 'b':1, 'c': 22}
 
 print sorted([(v, k) for k, v in c.items()])
 
-'''
+
+
+
