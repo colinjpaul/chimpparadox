8645756d1b5d9ffcc67bd82176e73aee9ff9ca5f
colinjpaul <colinpaul@gmail.com>
2016-09-07 13:35:29 +0100

    further changes
    got hours now to accumulate occurances

--- python_specialisation/Assignments/Assignment 10.2.py
+++ python_specialisation/Assignments/Assignment 10.2.py
@@ -3,14 +3,26 @@ if len(name) < 1:
     name = "C://Users//paulc1//Documents//GitHub//chimpparadox//testfiles/mbox-short.txt"
 handle = open(name)
 
+counts = dict()
+
 for line in handle:
     line = line.rstrip()
     if not line.startswith('From'):
         continue
     words = line.split()
 
-    if len(words[0]) == 4:
-        #split words[5]
+    if len(words[0]) != 4:
+        continue
+    word = words[5]
+    hour = word[0:2]
+
+
+
+
+
+
+
+
 
 
 
