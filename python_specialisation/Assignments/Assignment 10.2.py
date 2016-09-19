a9dfd7974d9bf1acc64d23028bf47bb3f677068b
colinjpaul <colinpaul@gmail.com>
2016-09-19 09:40:51 +0100

    assignment 10.2

--- python_specialisation/Assignments/Assignment 10.2.py
+++ python_specialisation/Assignments/Assignment 10.2.py
@@ -11,10 +11,43 @@ for line in handle:
         continue
     words = line.split()
 
+    # We want to exclude 'From:' (i.e. no colon)
     if len(words[0]) != 4:
         continue
+
+    # Extract Time
     word = words[5]
+    # Extract Hour
     hour = word[0:2]
+
+    # Store how many times each 'hour' occurs
+    counts[hour] = counts.get(hour, 0) + 1
+
+    # Create list to store key, value pairs so that they can be manipulated
+    lst = list()
+
+    # Populate list
+    for hour, count in counts.items():
+        lst.append((hour, count))
+
+
+
+#print sorted([(hour, count) for hour, count in counts.items()])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
