187a848ff7ad06c1a728d13322c048d1c8cfbc1e
colinjpaul <colinpaul@gmail.com>
2016-10-11 09:39:07 +0100

    regex_in_work

--- python_specialisation/Exercises/Chapter_11.py
+++ python_specialisation/Exercises/Chapter_11.py
@@ -1,6 +1,23 @@
 import re
 
-x = 'From: Using the : character'
-y = re.findall('^F.+?:', x)
+x = 'From colin.paul@dell.com Tue Oct'
+y = re.findall('^From .*@([^ ]*)', x)
 print y
+
+
+hand = open('C:\\users\\paulc1\\Documents\\GitHub\\chimpparadox\\testfiles\\mbox-short.txt')
+numlist = list()
+for line in hand:
+    line = line.rstrip()
+    stuff = re.findall('^X-DSPAM-Confidence: ([0-9.]+)', line)
+    if len(stuff) != 1:
+        continue
+    num = float(stuff[0])
+    numlist.append(num)
+
+print 'Maximum:', max(numlist)
+
+
+
+
 
