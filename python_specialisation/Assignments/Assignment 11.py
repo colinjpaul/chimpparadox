3c2e8117691df6239687b90b9c85dd5730f449b7
colinjpaul <colinpaul@gmail.com>
2016-10-19 09:30:18 +0100

    completed assignment 11

--- python_specialisation/Assignments/Assignment 11.py
+++ python_specialisation/Assignments/Assignment 11.py
@@ -1,18 +1,26 @@
 import re
 
-hand = open('C:\\users\\paulc1\\Documents\\GitHub\\chimpparadox\\testfiles\\regex_sum_42.txt')
-numlist = list()
+hand = open('C:\\users\\paulc1\\Documents\\GitHub\\chimpparadox\\testfiles\\regex_sum_271851.txt')
+total = 0
+
 for line in hand:
     line = line.rstrip()
     numFound = re.findall('[0-9]+', line)
-
     if len(numFound) < 1:
         continue
-        
     for num in numFound:
-        print num
-        numlist.append(num)
+        total += int(num)
 
-print numlist
+print total
 
-print 'Maximum:', max(numlist)
+'''
+
+Optional: Just for Fun
+There are a number of different ways to approach this problem. While we don't recommend trying to write the most compact code possible, it can sometimes be a fun exercise. Here is a a redacted version of two-line version of this program using list comprehension:
+
+import re
+print sum( [ ****** *** * in **********('[0-9]+',**************************.read()) ] )
+
+Please don't waste a lot of time trying to figure out the shortest solution until you have completed the homework. List comprehension is mentioned in Chapter 10 and the read() method is covered in Chapter 7.
+
+'''
