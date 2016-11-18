c6940eaf7af6ef6604070ed1011c84ddb870fe36
colinjpaul <colinpaul@gmail.com>
2016-11-18 06:48:17 +0000

    assign12_3_done

--- python_specialisation/Assignments/Assignment 12_3.py
+++ python_specialisation/Assignments/Assignment 12_3.py
@@ -5,7 +5,7 @@ from BeautifulSoup import *
 #  For all other purposes 'import bs4'
 
 #url = raw_input('Enter URL: ')
-url = 'http://python-data.dr-chuck.net/known_by_Fikret.html'
+url = 'http://python-data.dr-chuck.net/known_by_Leilan.html'
 
 currentCount = 0
 count = raw_input("Enter count: ")
@@ -13,6 +13,7 @@ count = int(count)
 
 position = raw_input("Enter position: ")
 position = int(position)
+position -= 1
 
 print('Retrieving: ' + url)
 
@@ -25,84 +26,3 @@ while currentCount < count:
     print ('Retrieving: ' + url)
     currentCount += 1
 
-
-
-
-'''
-html = urllib.urlopen(url).read()
-soup = BeautifulSoup(html)
-tags = soup('a')
-tag = tags[position]
-
-url = tag.get('href', None)
-print (url)
-
-
-html = urllib.urlopen(url).read()
-soup = BeautifulSoup(html)
-tags = soup('a')
-tag = tags[position]
-
-url = tag.get('href', None)
-print (url)
-
-
-html = urllib.urlopen(url).read()
-soup = BeautifulSoup(html)
-tags = soup('a')
-tag = tags[position]
-
-url = tag.get('href', None)
-print (url)
-
-
-
-
-
-
-
-
-
-def crawl(followme):
-    html = urllib.urlopen(followme).read()
-    soup = BeautifulSoup(html)
-    tags = soup('a')
-    for tag in tags:
-        print(tag.get('href', None))
-
-#url = raw_input('Enter URL: ')
-
-
-#count = raw_input("Enter count: ")
-position = raw_input("Enter position: ")
-
-#currentCount = 0
-currentPosition = 1
-
-if currentPosition == int(position):
-
-crawl(url)
-
-while currentCount < count:
-    if currentPosition == int(position):
-        print("Retrieving: " + url)
-        crawl(url)
-    currentPosition += 1
-currentCount += 1
-
-print("Retrieving: " + url)
-
-while currentCount < 4:
-    for tag in tags:
-        if currentPosition == int(position):
-            followMe = tag.get('href', None)
-            print("Retrieving: " + followMe)
-            html = urllib.urlopen(followMe).read()
-            soup = BeautifulSoup(html)
-            tags = soup('a')
-
-    if position == 3:
-        print tag.contents[0]
-
-    num = int(tag.contents[0])
-'''
