abdee8a87b0149f0d8e38298c78719aacd04a466
colinjpaul <colinpaul@gmail.com>
2016-12-02 10:55:48 +0000

    json_example_specialisation

--- python_specialisation/Resources/coursera_code/code/hidden.py
+++ python_specialisation/Resources/coursera_code/code/hidden.py
@@ -2,7 +2,7 @@
 # https://apps.twitter.com/
 
 def oauth() :
-    return { "consumer_key" : "h7Lu...Ng",
-        "consumer_secret" : "dNKenAC3New...mmn7Q",
-        "token_key" : "10185562-eibxCp9n2...P4GEQQOSGI",
-        "token_secret" : "H0ycCFemmC4wyf1...qoIpBo" }
+    return { "consumer_key" : "EysAe0hSnc3FbbhyJMfdaBf7a",
+        "consumer_secret" : "2bkkIRQhXPX68Vo8ZR30gIw2oISSyVkqejJZl8hgZikP2xztSO",
+        "token_key" : "84146547-xhgbwaWGPV7cjGhkGbbizhuS6sjHX0OUMQlt8EMFN",
+        "token_secret" : "pURjs4pm1gVoOOV9meatxPN7A4KsyfSaF9KAvnWKIRCXd" }
