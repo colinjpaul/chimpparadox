b565ddf5b9867cbd17503d3efd4d284dec2509ac
colinjpaul <colinpaul@gmail.com>
2016-12-16 10:06:50 +0000

    lpth_fri16dec16

---
+++ pythonthehardway/ex9.py
@@ -0,0 +1,11 @@
+days = 'Mon Tue Wed Thu Fri Sat Sun'
+months = '\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug'
+
+print 'Here are the days: ', days
+print 'Here are the months: ', months
+
+print '''
+There's something going on here
+With the double quotoes
+Can type as much as we like
+'''
