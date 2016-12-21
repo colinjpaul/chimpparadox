e30b62e2414fe698ffdb22ced1f257e6dfa190d6
colinjpaul <colinpaul@gmail.com>
2016-12-21 08:45:14 +0000

    latest_from_work

---
+++ pythonthehardway/ex11.py
@@ -0,0 +1,15 @@
+print 'how old are you?',
+age = raw_input()
+
+print 'how tall are you?',
+height = raw_input()
+
+print 'how much do you weigh',
+weight = raw_input()
+
+print 'so you\'re %r old and %r tall and weigh %r' %(age, height, weight)
+
+
+print 'enter a number',
+x = int(raw_input())
+print type(x)
---
+++ pythonthehardway/ex12.py
@@ -0,0 +1,5 @@
+age = raw_input('how old?')
+height = raw_input('how tall')
+weight = raw_input('what do you weigh')
+
+print 'so you\'re %r old, %r tall and weigh %r' % (age, height, weight)
