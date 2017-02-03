f9026e3e2901b3328268a9f2438a5f7a0c2ec357
colinjpaul <colinpaul@gmail.com>
2017-02-03 08:45:27 +0000

    update to tracks demo from work

--- python_specialisation/Exercises/chapter14/tracks.py
+++ python_specialisation/Exercises/chapter14/tracks.py
@@ -7,5 +7,56 @@ cur = conn.cursor()
 #Make tables
 cur.execute('''
 CREATE TABLE IF NOT EXISTS Artist (
-    id INTEGER NOT
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE)
 ''')
+
+cur.exexute('''
+CREATE TABLE IF NOT EXISTS Album (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    artist_id   INTEGER,
+    title   TEXT UNIQUE)
+''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Track (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title    TEXT UNIQUE,
+    album_id    INTEGER,
+    len INTEGER, rating INTEGER, count INTEGER)
+''')
+
+def lookup(d, key):
+    found = False
+    for child in d:
+        if found : return child.text
+        if child.tag == 'key' and child.text == key :
+            found = True
+    return None
+
+
+fname = raw_input('Enter file name: ')
+if (len(fname)<1) : fname = 'Library.xml'
+
+stuff = ET.parse(fname)
+all = stuff.findall('dict/dict/dict')
+print 'Dict count:', len(all)
+for entry in all:
+    if ( lookup(entry, 'Track ID') is None ) : continue
+
+    name = lookup(entry, 'Name')
+    artist = lookup(entry, 'Artist')
+    album = lookup(entry, 'Album')
+    count = lookup(entry, 'Play Count')
+    rating = lookup(entry, 'Rating')
+    length = lookup(entry, 'Total Time')
+
+    if name is None or artist is None or album is None :
+        continue
+
+    print name, artist, album, count, rating, length
+
+    cur.execute('''INSERT OR IGNORE INTO Artist (name)
+        VALUES ( ? )''', ( artist, ) )
+    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    artist_id = cur.fetchone()[0]
