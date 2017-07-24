f34ae889ea2c2740a0cba44309cc1a05bcaf0192
Colin Paul <colinpaul@gmail.com>
2017-07-24 13:53:11 +0100

    more stuff in java

--- Java_Cert_8/src/sandbox/TestClass.java
+++ Java_Cert_8/src/sandbox/TestClass.java
@@ -1,14 +1,9 @@
 public class TestClass {
     public static void main(String[] args) {
-        // Add test code here
-        // Add any print statements here
-        System.out.println("Hello World!");
 
-        float value1 =102;
-        float value2 = (int)102.0;
-        float value3 =1f * 0.0;
-        float value4 =1f * (short)0.0;
-        float value5 =1f * (boolean)0;
-
+        int x =0;
+        while(++x <5) { x+=1; }
+        String message = x >5 ? "Greater than" : "Less Than";
+        System.out.println(message+","+x);
     }
 }
