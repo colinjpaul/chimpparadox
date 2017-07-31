2e033c02d5f6e54824715690faeb594b1f23741d
colinjpaul <colinpaul@gmail.com>
2017-07-31 14:10:41 +0100

    javacert and data science in work

--- Java_Cert_8/src/sandbox/AssessmentTest.java
+++ Java_Cert_8/src/sandbox/AssessmentTest.java
@@ -1,17 +1,15 @@
 public class AssessmentTest{
     public static void main(String[] args){
-        boolean keepGoing = true;
-        int count = 0;
-        int x = 3;
-        while(count++ < 3){
-            int y = (1 + 2 * count) % 3;
-            switch(y){
-                default:
-                case 0: x -= 1; break;
-                case 1: x += 5;
+        System.out.print("a");
+        try {
+            System.out.print("b")
+            throw new IllegalArgumentException();
+            catch (RuntimeException e) {
+            System.out.print("c");
+            finally {
+                System.out.print("d");
             }
-        }
-        System.out.println(x);
+        System.out.print("e");
     }
 
 }
