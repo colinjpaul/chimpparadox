52ac208e6678222c6a9aa1e36b6c350550a78c97
colinjpaul <colinpaul@gmail.com>
2017-08-24 22:55:48 +0100

    changes from  home

---
+++ Java_Cert_8/src/sandbox/ExamQuestion.java
@@ -0,0 +1,10 @@
+package certification;
+
+class ExamQuestion {
+  //... code
+}
+
+interface MultipleChoice {
+  void choice1();
+  void choice2();
+}
---
+++ Java_Cert_8/src/sandbox/Multiple2.java
@@ -0,0 +1,17 @@
+interface Printable {
+    //... nothing here
+}
+
+class MyClass {
+    //... nothing here
+}
+
+interface Movable {
+    //... nothing here
+}
+
+class Car {
+    //... nothing here
+}
+
+public interface Multiple2{}
