6c93a41fedf006fe991899cbefe3c0dfadf868a7
colinjpaul <colinpaul@gmail.com>
2017-08-24 21:02:37 +0100

    commit home

---
+++ python_specialisation/Capstone/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','indiana.edu','cam.ac.uk','gmail.com','uct.ac.za','vt.edu','berkeley.edu','stanford.edu','tfd.co.uk','ucdavis.edu'],
+['2006',1658,638,599,432,428,399,403,265,0,302],
+['2007',1386,568,496,534,562,510,485,427,509,271]
+];
