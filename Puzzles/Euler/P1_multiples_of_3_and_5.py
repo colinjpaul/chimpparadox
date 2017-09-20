9a3dc95c937f78244845cf4d479d38165f628169
Colin Paul <colin.paul@dell.com>
2017-09-20 10:12:50 +0100

    puzzling times

--- Puzzles/Euler/P1_multiples_of_3_and_5.py
+++ Puzzles/Euler/P1_multiples_of_3_and_5.py
@@ -1,11 +1,11 @@
 
 def multiples_of_3_or_5():
-    for number in range(10):
+    for number in range(100):
         if not number % 3 or not number % 5:
             yield number
 print(sum(multiples_of_3_or_5()))
 
-print ("puzzles")
+
 
 
 
