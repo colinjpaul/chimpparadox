6e3658fa81405df4e06d525bc4b7699765c2be31
Colin Paul <colin.paul@dell.com>
2017-10-02 13:54:30 +0100

    chapter2_finally

---
+++ Java_Cert_8/Chapter2/sandbox_2.java
@@ -0,0 +1,14 @@
+
+class sandbox_2 {
+
+    public static void main(String[] args) {
+        byte num = 100;
+        short sum = 1240;
+        int total = 48764;
+        long population = 214748368;
+
+        System.out.println(population);
+    }
+
+
+}
