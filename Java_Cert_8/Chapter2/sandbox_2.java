10d4445ce6de9a7b1104c42c087edbae4da68bbc
colinjpaul <colinpaul@gmail.com>
2017-10-22 15:17:42 +0100

    working out data types

--- Java_Cert_8/Chapter2/sandbox_2.java
+++ Java_Cert_8/Chapter2/sandbox_2.java
@@ -2,22 +2,21 @@
 class sandbox_2 {
 
     public static void main(String[] args) {
-        byte num = 100;
-        short sum = 1240;
-        int total = 48764;
-        long population = 214748368;
-
-        System.out.println(population);
 
         int baseDecimal = 267;
         int octVal = 0413;
         int hexVal = 0x10B;
         int binVal = 0b100001011;
 
-        System.out.println(baseDecimal);
-        System.out.println(octVal);
-        System.out.println(hexVal);
-        System.out.println(binVal);
+        System.out.println(baseDecimal + octVal);
+        System.out.println(hexVal + binVal);
+
+        char c3 = (char)-122;
+
+        System.out.println(c3);
+
+        byte exam_total = 7;
+        System.out.println(exam_total);
 
 
     }
