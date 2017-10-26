cf0e0e53e7af6392b00bd293538d9e443909c8e2
colinjpaul <colinpaul@gmail.com>
2017-10-26 23:10:58 +0100

    some more progress

--- Java_Cert_8/Chapter2/sandbox_3.java
+++ Java_Cert_8/Chapter2/sandbox_3.java
@@ -1,24 +1,38 @@
 public class sandbox_3 {
 
     public static void main(String[] args){
-        Integer i1 = new Integer(10);
-        Integer i2 = new Integer(10);
+        /*Integer i1 = new Integer(100);
+        Integer i2 = new Integer(100);
 
-        Integer i3 = Integer.valueOf(10);
-        Integer i4 = Integer.valueOf(10);
+        Integer i3 = Integer.valueOf(100);
+        Integer i4 = Integer.valueOf(100);
 
-        Integer i5 = 10;
-        Integer i6 = 10;
+        Integer i5 = 100;
+        Integer i6 = 100;
 
-        System.out.println(i1 == i2);
+        System.out.println(i1.equals(i2));
         System.out.println(i3 == i4);
         System.out.println(i4 == i5);
         System.out.println(i5 == i6);
 
-        Integer var3 = Integer.valueOf(10);
-        Integer var4 = Integer.valueOf(10);
+        Integer obj1 = 100;
+        Short obj2 = 100;
 
-        System.out.println(var3 == var4);
+        System.out.println(obj1.equals(obj2));
+
+        Double d1 = new Double(12.67);
+        Double d2 = new Double(12.67);
+
+        System.out.println(d1 == d2);*/
+
+        ArrayList<Double> list = new ArrayList<Double>();
+        list.add(12.12);
+        list.add(11.24);
+        Double total = 0.0;
+        for (Double d : list)
+            total +=d;
+            System.out.println(total);
+
     }
 
 
