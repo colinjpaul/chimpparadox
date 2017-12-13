1ea8acddc19198e0dcee1b6aab207cd39e7f666a
Colin Paul <colin.paul@dell.com>
2017-12-13 13:35:34 +0000

    new file to learn unboxing - removed from sandbox_3.java

--- Java_Cert_8/Chapter2/sandbox_3.java
+++ Java_Cert_8/Chapter2/sandbox_3.java
@@ -27,16 +27,7 @@ public class sandbox_3 {
 
         System.out.println(d1 == d2);*/
 
-       /* ArrayList<Double> list = new ArrayList<Double>();
-        list.add(12.12);
-        list.add(11.24);
-        Double total = 0.0;
-        for (Double d : list)
-            total +=d;
-            System.out.println(total);*/
-
-
-       /* int myInt = 7;
+        int myInt = 7;
         boolean result = true;
         if (result == true)
             do
---
+++ Java_Cert_8/Chapter2/unboxing.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+
+public class unboxing{
+    public static void main (String args[]){
+        ArrayList<Double> list = new ArrayList<Double>();
+        list.add(12.12);
+        list.add(11.24);
+        Double total = 0.0;
+        for (Double d : list)
+            total += d;
+
+        System.out.println(total);
+
+    }
+
+
+
+
+}
