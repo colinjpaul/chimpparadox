2e8872c0cd631998cbe3411853ac03bde2dd355a
Colin <Colin_Paul@McAfee.com>
2018-10-23 14:10:37 +0100

    adding mindmaps

---
+++ mindmaps/What and When.mm
@@ -0,0 +1,107 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1540295221248" ID="ID_1745423130" MODIFIED="1540298948347" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Life 2.0
+    </p>
+    <p style="text-align: center">
+      What and When
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1540295772797" ID="ID_32649767" MODIFIED="1540300080990" POSITION="right" TEXT="Health" VSHIFT="-23">
+<icon BUILTIN="idea"/>
+<node CREATED="1540296546531" ID="ID_625417499" MODIFIED="1540299790250" TEXT="Running">
+<attribute NAME="When" VALUE="Hobby Time (8:30-10pm)"/>
+<attribute NAME="When" VALUE="Weekend"/>
+</node>
+<node CREATED="1540296554371" ID="ID_641145591" MODIFIED="1540299686557" TEXT="Walking">
+<attribute NAME="When" VALUE="Weekend"/>
+<attribute NAME="When" VALUE="Work (8:30am-5pm)"/>
+<attribute NAME="When" VALUE="Hobby Time (8:30-10pm)"/>
+</node>
+<node CREATED="1540296561912" ID="ID_689485034" MODIFIED="1540299782538" TEXT="Situps">
+<attribute NAME="When" VALUE="Hobby Time (8:30-10pm)"/>
+<attribute NAME="When" VALUE="Weekend"/>
+</node>
+<node CREATED="1540296578350" ID="ID_171994285" MODIFIED="1540299806559" TEXT="Diet">
+<attribute NAME="When" VALUE="Always"/>
+</node>
+</node>
+<node CREATED="1540295788025" HGAP="71" ID="ID_388331018" MODIFIED="1540299327446" POSITION="left" TEXT="Home" VSHIFT="2135">
+<node CREATED="1540296091907" HGAP="105" ID="ID_1664519490" MODIFIED="1540298838539" TEXT="Luke&apos;s Room" VSHIFT="8">
+<attribute NAME="When" VALUE="Weekend"/>
+</node>
+<node CREATED="1540296196699" HGAP="105" ID="ID_1856715108" MODIFIED="1540298842931" TEXT="Kitchen" VSHIFT="30">
+<attribute NAME="When" VALUE="Weekend"/>
+</node>
+<node CREATED="1540296226815" HGAP="110" ID="ID_404188842" MODIFIED="1540298851507" TEXT="Garden" VSHIFT="19">
+<attribute NAME="When" VALUE="Weekend"/>
+</node>
+<node CREATED="1540296244601" HGAP="74" ID="ID_628431916" MODIFIED="1540299107859" TEXT="Living Room" VSHIFT="-43">
+<attribute NAME="When" VALUE="Weekend"/>
+</node>
+</node>
+<node CREATED="1540295795888" HGAP="23" ID="ID_1126544970" MODIFIED="1540299466115" POSITION="right" TEXT="Work" VSHIFT="24">
+<node CREATED="1540296496562" HGAP="37" ID="ID_1490266684" MODIFIED="1540300108185" TEXT="Project &apos;Deploy&apos;" VSHIFT="1">
+<icon BUILTIN="idea"/>
+<attribute NAME="When" VALUE="Work (8:30am-5pm)"/>
+<attribute NAME="When" VALUE="Hobby Time (8:30-10pm)"/>
+</node>
+<node CREATED="1540299466116" HGAP="38" ID="ID_1091466273" MODIFIED="1540300112412" TEXT="Project &apos;Coverage&apos;" VSHIFT="34">
+<icon BUILTIN="idea"/>
+<attribute NAME="When" VALUE="Work (8:30am-5pm)"/>
+<attribute NAME="When" VALUE="Hobby Time (8:30-10pm)"/>
+</node>
+</node>
+<node CREATED="1540296037328" HGAP="21" ID="ID_323759101" MODIFIED="1540299739925" POSITION="left" TEXT="Hobbies" VSHIFT="-2081">
+<node CREATED="1540296368241" HGAP="26" ID="ID_1822044464" MODIFIED="1540298996791" TEXT="Golf" VSHIFT="523">
+<attribute NAME="When" VALUE="Sunday"/>
+</node>
+<node CREATED="1540296383027" HGAP="156" ID="ID_965307780" MODIFIED="1540300004623" TEXT="Drawing" VSHIFT="-58">
+<icon BUILTIN="idea"/>
+<attribute_layout VALUE_WIDTH="123"/>
+<attribute NAME="When" VALUE="Ealry Morning (6-7am)"/>
+<attribute NAME="When" VALUE="Hobby Time (8:30-10pm)"/>
+</node>
+<node CREATED="1540296391556" HGAP="176" ID="ID_125427135" MODIFIED="1540298209088" TEXT="Games" VSHIFT="-44">
+<attribute NAME="When" VALUE="Hobby Time (8:30-10pm)"/>
+</node>
+<node CREATED="1540296401931" HGAP="124" ID="ID_1891177810" MODIFIED="1540299004411" TEXT="Programming" VSHIFT="-10">
+<node COLOR="#000000" CREATED="1540296427596" ID="ID_1010303463" MODIFIED="1540300016954" TEXT="Programming Project">
+<icon BUILTIN="idea"/>
+<attribute_layout NAME_WIDTH="77" VALUE_WIDTH="166"/>
+<attribute NAME="When" VALUE="Hobby Time (8:30-10pm)"/>
+<attribute NAME="When" VALUE="Pre Work (7:30-8:30am)"/>
+</node>
+</node>
+<node CREATED="1540296596650" HGAP="194" ID="ID_952017367" MODIFIED="1540299207774" TEXT="Puzzles" VSHIFT="31">
+<attribute NAME="When" VALUE="Hobby Time (8:30-10pm)"/>
+</node>
+<node CREATED="1540296716274" HGAP="211" ID="ID_276306595" MODIFIED="1540300052676" TEXT="Luke&apos;s Toys" VSHIFT="11">
+<icon BUILTIN="idea"/>
+<attribute NAME="When" VALUE="Hobby Time (8:30-10pm)"/>
+<attribute NAME="When" VALUE="Weekend"/>
+</node>
+<node CREATED="1540298152406" HGAP="182" ID="ID_1508848322" MODIFIED="1540300057710" TEXT="Photos" VSHIFT="-321">
+<icon BUILTIN="idea"/>
+<attribute_layout VALUE_WIDTH="169"/>
+<attribute NAME="When" VALUE="Ealry Morning (6-7am)"/>
+<attribute NAME="When" VALUE="Hobby Time (8:30-10pm)"/>
+</node>
+<node CREATED="1540298587227" HGAP="444" ID="ID_735641423" MODIFIED="1540299277229" TEXT="TV" VSHIFT="-353">
+<attribute NAME="When" VALUE="Hobby Time (8:30-10pm)"/>
+<attribute NAME="When" VALUE="Weekend"/>
+</node>
+<node CREATED="1540299716602" HGAP="92" ID="ID_1636112671" MODIFIED="1540299734448" TEXT="Reading" VSHIFT="-229">
+<attribute NAME="When" VALUE="Bed Time (10-10:30pm)"/>
+</node>
+</node>
+</node>
+</map>
